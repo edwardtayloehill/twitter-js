b46e847e0751c238cbff8d61691271ee08f4b732
Edward Hill <edwardtayloehill@gmail.com>
2016-11-07 18:08:00 -0500

    Shows in browser

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 //we invoke express and now have an application
 const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+nunjucks.configure('views', { noCache: true });
 //set up a port
 const port = 3000;
 
@@ -27,3 +30,25 @@ app.get("/", (request,response) => {
 app.get("/news", (request,response) => {
   response.send("No news is good news")
 })
+
+app.get("/index", (request, response) => {
+  var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  response.render( 'index', {title: 'Hall of Fame', people: people} );
+})
+
+var locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+nunjucks.configure('views', {noCache: true});
+// nunjucks.render('index.html', locals, function (err, output) {
+//     console.log(output);
+// });
+
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
---
+++ views/index.html
@@ -0,0 +1,7 @@
+<h1>{{ title }}</h1>
+<p>This is a bare-bones template.</p>
+<ul>
+{% for person in people %}
+  <li>{{ person.name }}</li>
+{% endfor %}
+</ul>
